72dda94e255577d5262a044245a2d7e2b089b4bf
kana2022 <kkanat94@gmail.com>
2023-02-17 16:15:23 +0600

    changing this class

--- FirstNeuralNetwork/Neuron.cs
+++ FirstNeuralNetwork/Neuron.cs
@@ -2,25 +2,5 @@
 
 internal class Neuron
 {
-    private decimal weight = 0.5m; 
-    public decimal LastError { get; private set; }
-    public decimal Smoothing { get; set; } = 0.00001m;
-
-    public decimal ProcessInputData(decimal input)
-    {
-        return input * weight;
-    }
-
-    public decimal RestoreInputDate(decimal output)
-    {
-        return output * weight;
-    }
-
-    public void Train(decimal input, decimal expectedResult)
-    {
-        var actualResult = input * weight;
-        LastError = expectedResult - actualResult;
-        var correction = LastError / actualResult * Smoothing;
-        weight = weight + correction;
-    }
+    public List<double> Weights { get; }
 }
