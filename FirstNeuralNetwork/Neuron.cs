ef0cbb604845ed3e15d9774b5e51bad6ec5b0259
kana2022 <kkanat94@gmail.com>
2023-02-17 16:08:48 +0600

    already finished

--- FirstNeuralNetwork/Neuron.cs
+++ FirstNeuralNetwork/Neuron.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FirstNeuralNetwork;
 
-namespace FirstNeuralNetwork
+internal class Neuron
 {
-    internal class Neuron
+    private decimal weight = 0.5m; 
+    public decimal LastError { get; private set; }
+    public decimal Smoothing { get; set; } = 0.00001m;
+
+    public decimal ProcessInputData(decimal input)
     {
+        return input * weight;
+    }
+
+    public decimal RestoreInputDate(decimal output)
+    {
+        return output * weight;
+    }
+
+    public void Train(decimal input, decimal expectedResult)
+    {
+        var actualResult = input * weight;
+        LastError = expectedResult - actualResult;
+        var correction = LastError / actualResult * Smoothing;
+        weight = weight + correction;
     }
 }
